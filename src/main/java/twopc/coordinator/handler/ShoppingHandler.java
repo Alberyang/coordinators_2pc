a87936fcb05ab7ea7f7ad846cd003cb04d70bd41
whuo0002 <whuo0002@student.monash.edu>
2021-05-07 15:10:54 +0800

    coordinator ver 0.2

--- src/main/java/twopc/coordinator/handler/ShoppingHandler.java
+++ src/main/java/twopc/coordinator/handler/ShoppingHandler.java
@@ -1,12 +1,15 @@
 package twopc.coordinator.handler;
 
+import com.alibaba.fastjson.JSONObject;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.TimeUnit;
@@ -20,27 +23,39 @@ public class ShoppingHandler extends AbstractHandler {
     @Override
     public void handle(String target, Request baseRequest, HttpServletRequest request,
                        HttpServletResponse response) throws IOException, ServletException {
+        // Http Servlet Settings
+        request.setCharacterEncoding("UTF-8");
         response.setContentType("text/html;charset=utf-8");
         response.setCharacterEncoding("utf-8");
         baseRequest.setHandled(true);
 
+        if (target.equalsIgnoreCase("/shopping")) {
+            // Processing
+            String line;
+            StringBuilder jsonString = new StringBuilder();
+            BufferedReader reader = request.getReader();
+            while ((line = reader.readLine()) != null) {
+                jsonString.append(line);
+            }
+            JSONObject requestJson = JSONObject.parseObject(jsonString.toString());
 
-        if (target.equalsIgnoreCase("/transfer")) {
-            if (pre_commit() && do_commit()){
+            if (pre_commit(requestJson) && do_commit(requestJson)){
                 response.setStatus(HttpServletResponse.SC_OK);
                 log.info("Transaction Process Completed.");
             } else {
                 response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 log.info("Transaction Process Failed, Rollback to Previous");
             }
-
+        } else {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            log.info("Unknown type of operation");
         }
     }
 
     /**
      * Pre-Commit Phase
      */
-    private boolean pre_commit() {
+    private boolean pre_commit(JSONObject jsonData) {
         try{
             cyclicBarrier.await(3000, TimeUnit.MILLISECONDS);
         } catch (InterruptedException | BrokenBarrierException | TimeoutException e) {
@@ -61,9 +76,9 @@ public class ShoppingHandler extends AbstractHandler {
     }
 
     /**
-     * Pre-Commit Phase
+     * Do-Commit Phase
      */
-    private boolean do_commit() {
+    private boolean do_commit(JSONObject jsonData) {
         try{
             cyclicBarrier.await(3000, TimeUnit.MILLISECONDS);
         } catch (InterruptedException | BrokenBarrierException | TimeoutException e) {
